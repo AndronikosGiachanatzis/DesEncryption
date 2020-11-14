1a64fbd538a705d34796710a7cf6319c85acaed3
AndronikosGiachanatzis <a.giachanatzis@gmail.com>
2020-11-14 23:51:53 +0200

    Add splitKey

---
+++ des_tool.py
@@ -0,0 +1,33 @@
+import des
+
+# split the key in hex numbers
+def splitKey(s):
+
+    # get the bits
+    temp = [bin(int(s[i]+s[i+1], 16))[2:] for i in range(0, len(s), 2)]
+
+    # add padding
+    for i in range(len(temp)):
+        if len(temp[i]) < 8:
+            diff = 8-len(temp[i])
+            temp[i] = '0'*diff + temp[i]
+
+    # convert it to a list and discard the parity bits
+    key = []
+    for i in temp:
+        key.append([j for j in i[:-1]])
+    return key
+
+
+def main():
+    key = "133457799BBCDFF1"
+    plaintext = "GIACHANA"
+
+    # split the key into a 8x7 list (with the parity bits removed
+    key_list = splitKey(key)
+    print(key_list)
+
+
+
+if __name__ == "__main__":
+    main()
