c8fcc964ca59204e7fefa6b74ec0df8606b1374a
AndronikosGiachanatzis <a.giachanatzis@gmail.com>
2020-11-15 02:49:21 +0200

    add threading for initial calculations

--- des_tool.py
+++ des_tool.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python3
+'''
+Author: Andronikos Giachanatzis
+'''
+
 import des
 import destables
-
+import threading
+import time
+import concurrent.futures
 
 # split the key in hex numbers
 def splitKey(s):
@@ -21,6 +28,16 @@ def splitKey(s):
             key.append(bit)
     return key
 
+def getKeys(key):
+    # split the key and get the 16 subkeys
+    key_list = splitKey(key)
+    subkeys = des.key_schedule(key_list)
+    return subkeys
+
+
+def block_function_here():
+    time.sleep(2)
+    return ("val1", "val2")
 
 def main():
     # validation could be added but since input is hardcoded there is no need to do that
@@ -28,8 +45,22 @@ def main():
     plaintext = "GIACHANA"
 
     # split the key and get the 16 subkeys
-    key_list = splitKey(key)
-    subkeys = des.key_schedule(key_list)
+    print("starting thread")
+    future_subkeys = None
+    # create one thread for the creation of the 16 subkeys and another one for the
+    # initial block calculation
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_subkeys = executor.submit(getKeys, key)
+        # thread for blocks
+        future = executor.submit(block_function_here)
+
+    # get the subkeys
+    subkeys = future_subkeys.result()
+
+
+    print("thread finished")
+
+
 
 
 
